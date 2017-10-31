e1ab2ab0376691b5ffed18d4d34c4511d4448101
Emery <Darrick.Emery@stlcop.local>
2017-10-31 18:14:06 -0500

    4.R Q1-3

--- 4.R
+++ 4.R
@@ -2,3 +2,8 @@ dimorph_data <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian
                 sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999", "-999.0"))
 ggplot(data= dimorph_data, aes(x= F_mass))+
   labs(x= "Female Mass(g)") + geom_histogram()
+ggplot(data= dimorph_data, aes(x= F_mass))+
+  labs(x= "Female Mass(g)") + geom_histogram(fill= "blue")
+ggplot(data = dat, aes(x = F_mass)) +
+  geom_histogram(fill = "blue", alpha = 0.3) + geom_histogram(aes(x = M_mass), alpha = 0.3) +
+  scale_x_log10() + labs(x = "Female Mass(g)") 
