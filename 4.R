4133a852820e52ea0142dcb7743588e7aac0d730
Emery <Darrick.Emery@stlcop.local>
2017-10-31 18:20:33 -0500

    Revert "Revert "4.R Q1-3""
    This reverts commit 352ff56fe9df7da2ead4a00509703724c5a1c3c1.

--- 4.R
+++ 4.R
@@ -1,7 +1,6 @@
 dimorph_data <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan07.txt"),
                 sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999", "-999.0"))
 ggplot(data= dimorph_data, aes(x= F_mass))+
-<<<<<<< HEAD
   labs(x= "Female Mass(g)") + geom_histogram()
 ggplot(data= dimorph_data, aes(x= F_mass))+
   labs(x= "Female Mass(g)") + geom_histogram(fill= "blue")
@@ -16,6 +15,3 @@ ggplot(data = dimorph_data, aes(x = F_wing)) +
   geom_histogram(fill = "blue", alpha = 0.3) + geom_histogram(aes(x = M_wing), alpha = 0.3) +
   scale_x_log10() + labs(x = "Female Mass(g)") +
   facet_wrap(~ Family)
-=======
-  labs(x= "Female Mass(g)") + geom_histogram()
->>>>>>> parent of e1ab2ab... 4.R Q1-3
