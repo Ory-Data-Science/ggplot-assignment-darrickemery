d1c7956446a46ba3e2eda5de9bc225721fdf097f
Emery <Darrick.Emery@stlcop.local>
2017-10-31 18:03:37 -0500

    3.R Q1-5 Complete

--- 3.R
+++ 3.R
@@ -11,3 +11,12 @@ ggplot(data= mammal_data, aes(x= mass.g., y= newborn.g., color= order)) +
   labs(x= "Adult Mass", y= "Newborn Mass") +
   scale_x_log10()+ scale_y_log10() +
   geom_point()
+ggplot(data= mammal_data, aes(x=mass.g., y= newborn.g.)) +
+  labs(x= "Adult Mass", y= "Newborn Mass") +
+  scale_x_log10() + scale_y_log10() +
+  geom_point() + facet_wrap(~ order, nrow = 4, ncol = 5)
+ggplot(data= mammal_data, aes(x=mass.g., y= newborn.g.)) +
+  labs(x= "Adult Mass", y= "Newborn Mass") +
+  scale_x_log10() + scale_y_log10() +
+  geom_point() + geom_smooth(method = "lm") +
+  facet_wrap(~ order, nrow = 4, ncol = 5)
